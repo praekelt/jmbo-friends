2f59222f4ce9956bd67671c4b383dc62ba371a9e
Euan Jonker <euan@unomena.com>
2012-05-03 15:09:22 +0200

    Upped version to 0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jmbo-friends',
-    version='0.0.5',
+    version='0.0.6',
     description='Friends and badges for Jmbo.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
