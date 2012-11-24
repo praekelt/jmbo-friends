c37fa1f20a1ee2d61a9437a59faffd9fad5bf6da
Rizmari Versfeld <rizziepit@gmail.com>
2012-11-25 01:46:34 +0200

    forgot settings import

--- friends/models.py
+++ friends/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _, ugettext
+from django.conf import settings
 
 from foundry.models import Member, Link, Notification
 
