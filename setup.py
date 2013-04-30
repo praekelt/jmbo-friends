523aae605f517f3d1f7c7ac52fea9afec4d7a6e6
hedleyroos <hedleyroos@gmail.com>
2013-04-30 15:48:42 +0200

    Prep for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jmbo-friends',
-    version='0.2.1',
+    version='0.2.2',
     description='Friends and badges for Jmbo.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
